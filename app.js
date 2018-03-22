fb8ada5448fae2904d9a5aad4ccb0769cf8b1a22
Matt Carmody <mcarmody90@yahoo.com>
2018-03-22 18:21:45 +0000

    update dburl

--- app.js
+++ app.js
@@ -18,7 +18,9 @@ var campgroundRoutes    = require("./routes/campgrounds"),
     
     
 // mongoose.connect("mongodb://localhost/yelp_camp_v12deployed");
-mongoose.connect("mongodb://matt:domino@ds215709.mlab.com:15709/yelp_camp");
+// mongoose.connect("mongodb://matt:domino@ds215709.mlab.com:15709/yelp_camp");
+mongoose.connect(process.env.DATABASEURL);
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
